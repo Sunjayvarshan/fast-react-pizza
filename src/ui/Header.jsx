e5baf563b50844b870401c655ca855e8449a93f2
Sunjay Varshan <111437799+Sunjayvarshan@users.noreply.github.com>
2023-07-25 22:11:03 +0530

    initializing the Routes

---
+++ src/ui/Header.jsx
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom";
+
+function Header() {
+  return (
+    <header>
+      <Link to="/">Fast React Pizza Co.</Link>
+      <p>hello</p>
+    </header>
+  );
+}
+
+export default Header;
